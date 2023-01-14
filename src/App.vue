c85048edbfdc86a2cc2630d60ac37375e1cecd6c
SebastianoUrban <sebastiano.urban98@gmail.com>
2023-01-14 17:54:30 +0100

    first standard query to API

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,50 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<template>
+    <div>
+      <h1>Risultati</h1>
+      <ol>
+        <li v-for="item in output" :key="item.id">
+          <ul>
+            <li>Titolo: {{ item.title }}</li>
+            <li>Titolo originale: {{ item.original_title }}</li>
+            <li>Lingua originale: {{ item.original_language }}</li>
+            <li>Voto medio: {{ item.vote_average }}</li>
+          </ul>
+        </li>
+      </ol>
+    </div>
+  </template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    data() {
+      return {
+        output : []
+      }
+    },methods: {
+      query() {
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=fd496a5d9593ae9ca1b97ac4b05d38cf&query=ritorno al futuro')
+          .then( (response) => {
+            console.log(response);
+            this.output = response.data.results;
+          })
+          .catch( (error) => {
+            console.log('ERROR');
+            console.log(error);
+          })
+      }
+    },
+    created () {
+      this.query();
+    },
+  }
 </script>
 
-<template>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+<style lang="scss">
+  @import "/node_modules/bootstrap/scss/bootstrap";
 
-<style scoped>
-
+  ol>li {
+    margin: 1rem 0;
+  }
 </style>
