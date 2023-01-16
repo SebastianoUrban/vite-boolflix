99cbf747c7c86166bb0d7fbd09052854d77afc23
SebastianoUrban <sebastiano.urban98@gmail.com>
2023-01-16 17:38:45 +0100

    add query tv show

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="wrapper">
       <input type="text" name="search" v-model="store.searchText">
-      <button @click="queryMovie()" >Cerca</button>
+      <button @click="queryTv()" >Cerca</button>
       <h1>Risultati</h1>
       <ol>
-        <li v-for="item in store.results" :key="item.id">
+        <li v-for="item in store.results" :key="item.title">
           <ul>
             <li>Titolo: {{ item.title }}</li>
-            <li>Titolo originale: {{ item.original_title }}</li>
-            <li>Lingua originale: {{ item.original_language }} <img class="flag" :src="getFlag(item.original_language)" alt=""></li>
-            <li>Voto medio: {{ item.vote_average }}</li>
+            <li>Titolo originale: {{ item.originalTitle }}</li>
+            <li>Lingua originale: {{ item.language }} <img class="flag" :src="getFlag(item.language)" alt=""></li>
+            <li>Voto medio: {{ item.rateing }}</li>
           </ul>
         </li>
       </ol>
@@ -30,8 +30,32 @@
         axios.get('https://api.themoviedb.org/3/search/movie?api_key=fd496a5d9593ae9ca1b97ac4b05d38cf&query='+store.searchText)
           .then( (response) => {
             console.log(response);
-            store.results = response.data.results;
-            store.searchText = '';
+            response.data.results.forEach(film => {
+              store.results.push({
+                title : film.title,
+                originalTitle : film.original_title,
+                language : film.original_language,
+                rateing : film.vote_average
+              });
+            });
+          })
+          .catch( (error) => {
+            console.log('ERROR');
+            console.log(error);
+          })
+      },
+      queryTv() {
+        axios.get('https://api.themoviedb.org/3/search/tv?api_key=fd496a5d9593ae9ca1b97ac4b05d38cf&language=it_IT&query='+store.searchText)
+          .then( (response) => {
+            console.log(response);
+            response.data.results.forEach(tv => {
+              store.results.push({
+                title : tv.name,
+                originalTitle : tv.original_name,
+                language : tv.original_language,
+                rateing : tv.vote_average
+              });
+            });
           })
           .catch( (error) => {
             console.log('ERROR');
