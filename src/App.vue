b7b395550b1db4f9ac6d4b0c4470c549e1c17d53
SebastianoUrban <sebastiano.urban98@gmail.com>
2023-01-16 17:15:21 +0100

    added flags

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="wrapper">
-      <input type="text" name="search" v-model="searchText">
-      <button @click="query()" >Cerca</button>
+      <input type="text" name="search" v-model="store.searchText">
+      <button @click="queryMovie()" >Cerca</button>
       <h1>Risultati</h1>
       <ol>
         <li v-for="item in store.results" :key="item.id">
           <ul>
             <li>Titolo: {{ item.title }}</li>
             <li>Titolo originale: {{ item.original_title }}</li>
-            <li>Lingua originale: {{ item.original_language }}</li>
+            <li>Lingua originale: {{ item.original_language }} <img class="flag" :src="getFlag(item.original_language)" alt=""></li>
             <li>Voto medio: {{ item.vote_average }}</li>
           </ul>
         </li>
@@ -23,21 +23,27 @@
   export default {
     data() {
       return {
-        store,
-        searchText : ''
+        store
       }
     },methods: {
-      query() {
-        axios.get('https://api.themoviedb.org/3/search/movie?api_key=fd496a5d9593ae9ca1b97ac4b05d38cf&query='+this.searchText)
+      queryMovie() {
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=fd496a5d9593ae9ca1b97ac4b05d38cf&query='+store.searchText)
           .then( (response) => {
             console.log(response);
             store.results = response.data.results;
+            store.searchText = '';
           })
           .catch( (error) => {
             console.log('ERROR');
             console.log(error);
           })
-        this.searchText = '';
+      },
+      getFlag (code) {
+        if (code in store.flags) {
+          return store.flags[code]
+        } else {
+          return store.flags.xx
+        }
       }
     },
     created () {
