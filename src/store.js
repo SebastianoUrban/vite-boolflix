9e3eb3af756b26d331b02ea02b064545efb81877
SebastianoUrban <sebastiano.urban98@gmail.com>
2023-01-16 17:14:53 +0100

    added flags

--- src/store.js
+++ src/store.js
@@ -1,5 +1,21 @@
 import { reactive} from 'vue';
 
 export const store = reactive({
-    results : []
+    results : [],
+    searchText : '',
+    flags : {
+        xx : '../src/assets/flags/4x3/xx.svg',
+        de : '../src/assets/flags/4x3/de.svg',
+        fr : '../src/assets/flags/4x3/fr.svg',
+        it : '../src/assets/flags/4x3/it.svg',
+        en : '../src/assets/flags/4x3/us.svg',
+        es : '../src/assets/flags/4x3/es.svg',
+        sv : '../src/assets/flags/4x3/sv.svg',
+        vi : '../src/assets/flags/4x3/vn.svg',
+        hi : '../src/assets/flags/4x3/in.svg',
+        zh : '../src/assets/flags/4x3/cn.svg',
+        cn : '../src/assets/flags/4x3/cn.svg',
+        pl : '../src/assets/flags/4x3/pl.svg',
+        pt : '../src/assets/flags/4x3/pt.svg'
+    }
 });
